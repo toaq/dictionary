6a0fc7853faa0f8c6ce9a93177d4a6f7f39d6a2f
ciuak <ciuak.prog@gmail.com>
2019-06-29 01:17:31 +0200

    preparation for the salvation

--- html/make.js
+++ html/make.js
@@ -16,16 +16,34 @@ function render_entry(entry) {
     if(entry.frame !== undefined)
       throw new Error(`frame ‘${entry.frame}’ from entry «${entry.toaq}» does not have a namesake`);
   } else attr('frame', `${frame_name} (${entry.frame})`);
+  let english = entry.english;
+  if(entry.distribution) {
+    english = english.split('; ');
+    let e = entry.distribution.split('; ').pop().split(' ');
+    let last_english = english.pop().split('▯')
+      .map((part, i) => {
+        if(i == e.length) return part;
+        return part + `▯<sub class="distribution">${e[i]}</sub>`;
+      }).join('');
+    english.push(last_english);
+    english = english.join('; ');
+  }
   if(entry.examples && entry.examples.length) {
     let maples = entry.examples.map(({toaq, english}) =>
       `<li><span class="toaq">${toaq}</span> <span class="english">${english}</span></li>`);
     attr('examples', `<ul class="examples">${maples.join(' ')}</ul>`);
   }
-  if(entry.notes && entry.notes.length) {
-    attr('notes', `<ul class="notes"><li>${entry.notes.join('</li> <li>')}</li></ul>`);
-  }
+  if(entry.fields && entry.fields.length)
+    entry.fields = entry.fields.map((_, i) =>
+      '<span class="toaq">' +
+      _.map((s, i) => s.replace(/[auıoe]/,
+        m => ((m == 'ı' ? 'i' : m) + '\u0309').normalize('NFC'))).join(' ra ')
+      + ' dó' + ['shī', 'gū', 'sāq', 'jō', 'fē', 'cī'][i] + '</span>');
+  for(f of ['notes', 'keywords', 'fields'])
+    if(entry[f] && entry[f].length)
+      attr(f, `<ul class="notes"><li>${entry[f].join('</li> <li>')}</li></ul>`);
   if(entry.gloss) gl = ` <span class="gloss">‘${entry.gloss}’</span>`;
-  return `<div class="entry"><div class="header"><span class="toaq">${entry.toaq}</span> <span class="type">${entry.type}</span>${gl || ''}</div> <div class="content">${entry.english}</div> <ul class="footer">${attributes.join(' ')}</ul></div>`;
+  return `<div class="entry"><div class="header"><span class="toaq">${entry.toaq}</span> <span class="type">${entry.type}</span>${gl || ''}</div> <div class="content">${english}</div> <ul class="footer">${attributes.join(' ')}</ul></div>`;
 }
 
 const template = fs.readFileSync('template.html').toString();
