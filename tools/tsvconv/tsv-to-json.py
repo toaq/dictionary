ce0f580ecc2b61910683e11e2cf220d79baf7c56
Ntsekees <ntsekees@users.noreply.github.com>
2023-11-19 12:51:15 +0100

    ⟦tsv-to-json.py⟧: Ensuring that the output JSON is conformant to ⟦normalize.js⟧'s standards.

--- tools/tsvconv/tsv-to-json.py
+++ tools/tsvconv/tsv-to-json.py
@@ -16,14 +16,28 @@ def entrypoint():
 		SELF_PATH + normalized("/dictionary.tsv"),
 		delimiter = "\t")
 	header, content = table[0], table [1:]
-	data = [
-		transformed({header[i] : e for i, e in enumerate(row)})
-		for row in content
-	]
-	save_as_json_file(
-		data, SELF_PATH + normalized("/../../dictionary.json"))
+	s = {"toaq", "type", "english"} - set(header)
+	if s != set():
+		n = "s" if len(s) != 1 else ""
+		print(f"⚠ ERROR: Mandatory column{n} headers {s} missing!")
+	else:
+		data = [
+			transformed({header[i] : e for i, e in enumerate(row)})
+			for row in content
+		]
+		save_as_json_file(
+			data, SELF_PATH + normalized("/../../dictionary.json"))
 	print("Execution time: {:.3f}s.".format(
 		time.time() - start_time))
+
+VERB_TYPES = {
+  "predicate",
+  "predicatizer",
+  "name verb",
+  "name quote",
+  "word quote",
+  "text quote",
+}
 
 def transformed(entry):
 	def f(l):
@@ -33,7 +47,17 @@ def transformed(entry):
 	entry["notes"] = f(entry.get("notes", "").split("\n"))
 	entry["keywords"] = f(entry.get("keywords", "").split("; "))
 	entry["fields"] = f(entry.get("fields", "").split("; "))
-	return reordered(entry)
+	
+	return reordered(normalized(entry))
+
+def normalized(entry):
+	verb_only_fields = {
+		"frame", "distribution", "pronominal_class", "subject", "verb_class", "namesake", "notes", "examples", "fields"
+	}
+	for f in verb_only_fields:
+		if f in entry.keys():
+			entry.pop(f)
+	return entry
 
 def reordered(entry):
 	order = (
