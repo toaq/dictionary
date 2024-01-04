86578de8d1a1bd62ddddd4b89b164b783c457ddc
Lynn <lynn@foldr.moe>
2024-01-04 02:24:23 +0100

    Sort even more alphabetically

--- tools/normalize.js
+++ tools/normalize.js
@@ -7,33 +7,21 @@ process.chdir(__dirname);
 let d = require("./../dictionary.json");
 
 function sortify(s) {
-  return (
-    s
-      .normalize("NFD")
-      .toLowerCase()
-      .replace(/ı/g, "i")
-      .replace(/ꝡ/g, "v")
-      .replace(/[^a-z\ ]/g, "")
-      // Handle the empty onset.
-      .replace(/(?<= |^)(?=[aeiouy])/, "'")
-  );
+  return s
+    .normalize("NFD")
+    .toLowerCase()
+    .replace(/ı/g, "i")
+    .replace(/ꝡ/g, "v")
+    .replace(/[^a-z\ ]/g, "");
 }
 
 d = d.sort((a, b) => {
-  const [aToaq, bToaq] = [a.toaq, b.toaq].map(sortify);
   if (a.toaq === b.toaq)
     throw new Error(`duplicate entries: «${a.toaq}» and «${b.toaq}»!`);
 
-  let [aParts, bParts] = [aToaq, bToaq].map((_) =>
-    _.split(/(?<=[aeiouy`])(?=[^aeiouy`])/)
-  );
-  while (aParts.length || bParts.length) {
-    const [aPart, bPart] = [aParts.shift(), bParts.shift()];
-    if (!aPart || !bPart) return +aPart - +bPart;
-    if (aPart < bPart) return -1;
-    if (aPart > bPart) return 1;
-  }
-  return 0;
+  const ka = sortify(a.toaq);
+  const kb = sortify(b.toaq);
+  return ka < kb ? -1 : ka > kb ? 1 : 0;
 });
 
 const verbyTypes = [
